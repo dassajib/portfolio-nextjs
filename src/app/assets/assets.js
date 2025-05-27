d0db511b5955de8e786ba2fd8d9f9290470bc28e
dassajib <sajibdas.sd@gmail.com>
2025-05-28 02:06:31 +0600

    navbar design

--- src/app/assets/assets.js
+++ src/app/assets/assets.js
@@ -76,8 +76,29 @@ export const assets = {
     right_arrow_bold_dark
 };
 
-export const workData = [
+export const navLinks = [{
+        label: "Home",
+        href: "#top"
+    },
     {
+        label: "About Me",
+        href: "#top"
+    },
+    {
+        label: "Services",
+        href: "#top"
+    },
+    {
+        label: "My Work",
+        href: "#work"
+    },
+    {
+        label: "Contact",
+        href: "#contact"
+    },
+];
+
+export const workData = [{
         title: 'Frontend project',
         description: 'Web Design',
         bgImage: '/work-1.png',
@@ -99,17 +120,50 @@ export const workData = [
     },
 ]
 
-export const serviceData = [
-    { icon: assets.web_icon, title: 'Web design', description: 'Web development is the process of building, programming...', link: '' },
-    { icon: assets.mobile_icon, title: 'Mobile app', description: 'Mobile app development involves creating software for mobile devices...', link: '' },
-    { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless user experience...', link: '' },
-    { icon: assets.graphics_icon, title: 'Graphics design', description: 'Creative design solutions to enhance visual communication...', link: '' },
+export const serviceData = [{
+        icon: assets.web_icon,
+        title: 'Web design',
+        description: 'Web development is the process of building, programming...',
+        link: ''
+    },
+    {
+        icon: assets.mobile_icon,
+        title: 'Mobile app',
+        description: 'Mobile app development involves creating software for mobile devices...',
+        link: ''
+    },
+    {
+        icon: assets.ui_icon,
+        title: 'UI/UX design',
+        description: 'UI/UX design focuses on creating a seamless user experience...',
+        link: ''
+    },
+    {
+        icon: assets.graphics_icon,
+        title: 'Graphics design',
+        description: 'Creative design solutions to enhance visual communication...',
+        link: ''
+    },
 ]
 
-export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+export const infoList = [{
+        icon: assets.code_icon,
+        iconDark: assets.code_icon_dark,
+        title: 'Languages',
+        description: 'JavaScript, React Js, Next Js, Golang, Python, HTML, CSS'
+    },
+    {
+        icon: assets.edu_icon,
+        iconDark: assets.edu_icon_dark,
+        title: 'Education',
+        description: 'B.Sc. in Software Engineering'
+    },
+    {
+        icon: assets.project_icon,
+        iconDark: assets.project_icon_dark,
+        title: 'Projects',
+        description: 'Built more than 5 projects'
+    }
 ];
 
 export const toolsData = [
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,7 +1,57 @@
+"use client"
+
+import { useState } from "react"
+import Image from "next/image"
+import { assets, navLinks } from "../assets/assets"
+
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    const [open, setOpen] = useState(false)
+
+    return (
+        <nav className="w-full fixed z-50 px-5 lg:px-8 xl:px-[8%] py-4 flex justify-between items-center">
+            <a href="#top">
+                <Image src={assets.logo} alt="logo" className="w-32 cursor-pointer" />
+            </a>
+
+            <ul className="hidden md:flex items-center gap-6 px-10 py-3 rounded-full lg:gap-8 bg-white/50 shadow-sm">
+                {
+                    navLinks.map(({ label, href }) => (
+                        <li key={label}>
+                            <a href={href}> {label} </a>
+                        </li>
+                    ))
+                }
+            </ul>
+
+            <div className="flex items-center gap-4">
+                <button>
+                    <Image src={assets.moon_icon} alt="moon icon" className="w-6 cursor-pointer" />
+                </button>
+                <a href="#contact" className="hidden md:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4">
+                    Contact Me
+                    <Image src={assets.arrow_icon} alt="contact logo" className="w-3" />
+                </a>
+                <button className="block md:hidden ml-3 cursor-pointer">
+                    <Image onClick={() => setOpen(!open)} src={assets.menu_black} alt="menu" className="w-6" />
+                </button>
+            </div>
+
+            {/* mobile menu */}
+            <ul className={`flex flex-col gap-4 py-20 px-10 bg-rose-50 fixed ${open ? "-right-0" : "-right-64"} top-0 bottom-0 w-56 h-screen transition duration-500`}>
+                <div className="absolute top-6 right-6">
+                    <Image onClick={() => setOpen(!open)} src={assets.close_black} alt="close image" className="w-6 cursor-pointer" />
+                </div>
+
+                {
+                    navLinks.map(({ label, href }) => (
+                        <li key={label}>
+                            <a href={href}>{label}</a>
+                        </li>
+                    ))
+                }
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
